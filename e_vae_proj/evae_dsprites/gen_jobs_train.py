737b096d94f54f760b53adda6913d7b66ab18fe4
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2021-07-02 13:33:28 +0100

    eps

--- e_vae_proj/evae_dsprites/gen_jobs_train.py
+++ e_vae_proj/evae_dsprites/gen_jobs_train.py
@@ -1,69 +1,54 @@
 import numpy as np
 from pathlib import Path
+import sys
 
 if __name__ == '__main__':
+    # constraint type
+    cons = sys.argv[1]
+    assert cons in ['kl', 'rec']
+
     # absolute path
     my_path = Path(__file__).parent.resolve().expanduser()
     main_path = my_path.parent.parent
 
     # read
-    epsilons_recon = np.loadtxt(my_path / 'grid_epsilons_recon')
-    epsilons_kl = np.loadtxt(my_path / 'grid_epsilons_kl')
+    epses = np.loadtxt(my_path / f'grid_eps_{cons}')
     nlats = np.loadtxt(my_path / 'grid_nlats')
     seed = 0
 
     # cmd template
     cmd_tmp = f'singularity exec --nv {str(main_path)}/../torch.simg ' \
-              f'python main.py evae_dsprites/z%d_b%s_s{seed} -s {seed} ' \
+              f'python main.py evae_dsprites_{cons}/z%d_e%s_s{seed} -s {seed} ' \
               f'--checkpoint-every 25 -d dsprites -e 50 -b 256 --lr 0.0005 ' \
-              f'-z %d -l epsilonH --epsilonH-B %s --is-metrics --no-test --no-progress-bar\n'
+              f'-z %d -l epsvae --epsvae-constrain-reconstruction {cons == "rec"} ' \
+              f'--epsvae-epsilon %s --is-metrics --no-test --no-progress-bar\n'
 
     # job header
     with open(my_path / 'job_header', 'r') as f:
         job_header = f.read()
 
-    # jobs for constrained kl, grouped by epsilon
-    for iepsilon, epsilon in enumerate(epsilons_kl):
-        with open(my_path / f'jobs/epsilon_kl{iepsilon}.job', 'w') as f:
-            h = job_header.replace('@JNAME@', f'evae_dsprites_epsilon_kl{iepsilon}')
-            h = h.replace('@OUTPUT@', str(my_path / f'jobs/epsilon_kl{iepsilon}.slurm'))
+    # jobs grouped by eps
+    for ieps, eps in enumerate(epses):
+        with open(my_path / f'jobs_{cons}/eps{ieps}.job', 'w') as f:
+            h = job_header.replace('@JNAME@', f'{cons}_e{ieps}')
+            h = h.replace('@OUTPUT@', str(my_path / f'jobs_{cons}/eps{ieps}.slurm'))
             h = h.replace('@MAINPATH@', str(main_path))
             f.write(h)
             for nlat in nlats:
-                unnormalized_epsilon = epsilon * 64 * 64 / nlat
-                cmd = cmd_tmp % (nlat, str(epsilon), nlat, str(unnormalized_epsilon))
+                if cons == 'rec':
+                    unnormalized_eps = eps * 64 * 64
+                else:
+                    unnormalized_eps = eps * nlat
+                cmd = cmd_tmp % (nlat, str(eps), nlat, str(unnormalized_eps))
                 f.write(cmd)
 
-    # jobs for constrained recon, grouped by epsilon
-    for iepsilon, epsilon in enumerate(epsilons_recon):
-        with open(my_path / f'jobs/epsilon_recon{iepsilon}.job', 'w') as f:
-            h = job_header.replace('@JNAME@', f'evae_dsprites_epsilon_recon{iepsilon}')
-            h = h.replace('@OUTPUT@', str(my_path / f'jobs/epsilon_recon{iepsilon}.slurm'))
-            h = h.replace('@MAINPATH@', str(main_path))
-            f.write(h)
-            for nlat in nlats:
-                unnormalized_epsilon = epsilon * 64 * 64 / nlat
-                cmd = cmd_tmp % (nlat, str(epsilon), nlat, str(unnormalized_epsilon))
-                f.write(cmd)
+    # submit
+    with open(my_path / f'jobs_{cons}/submit0.sh', 'w') as f:
+        for ieps, eps in enumerate(epses):
+            if ieps % 2 == 0:
+                f.write(f'sbatch {str(my_path)}/jobs_{cons}/eps{ieps}.job\n')
 
-    # submit constrained kl
-    with open(my_path / 'jobs/submit0_kl.sh', 'w') as f:
-        for iepsilon, epsilon in enumerate(epsilons_kl):
-            if iepsilon % 2 == 0:
-                f.write(f'sbatch {str(my_path)}/jobs/epsilon_kl{iepsilon}.job\n')
-
-    with open(my_path / 'jobs/submit1_kl.sh', 'w') as f:
-        for iepsilon, epsilon in enumerate(epsilons_kl):
-            if iepsilon % 2 == 1:
-                f.write(f'sbatch {str(my_path)}/jobs/epsilon_kl{iepsilon}.job\n')
-
-    # submit constrained recon
-    with open(my_path / 'jobs/submit0_recon.sh', 'w') as f:
-        for iepsilon, epsilon in enumerate(epsilons_recon):
-            if iepsilon % 2 == 0:
-                f.write(f'sbatch {str(my_path)}/jobs/epsilon_recon{iepsilon}.job\n')
-
-    with open(my_path / 'jobs/submit1_recon.sh', 'w') as f:
-        for iepsilon, epsilon in enumerate(epsilons_recon):
-            if iepsilon % 2 == 1:
-                f.write(f'sbatch {str(my_path)}/jobs/epsilon_recon{iepsilon}.job\n')
+    with open(my_path / f'jobs_{cons}/submit1.sh', 'w') as f:
+        for ieps, eps in enumerate(epses):
+            if ieps % 2 == 1:
+                f.write(f'sbatch {str(my_path)}/jobs_{cons}/eps{ieps}.job\n')
