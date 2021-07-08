a4fdeff0360c263cac1445d98fec97a680dff8b4
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2021-07-08 20:38:43 +0100

    job

--- e_vae_proj/bvae_dsprites/run.sh
+++ e_vae_proj/bvae_dsprites/run.sh
@@ -4,9 +4,9 @@
 #SBATCH --mem-per-cpu=32G
 #SBATCH --mem-per-gpu=32G
 #SBATCH --threads-per-core=1
-#SBATCH -n 4
-#SBATCH --gres=gpu:4
-#SBATCH -t 00:10:00
+#SBATCH -n 8
+#SBATCH --gres=gpu:8
+#SBATCH -t 5-00:00:00
 
 module load OpenMPI/4.1.0-GCC-9.3.0
 mpirun --mca btl_openib_allow_ib 1 --mca btl_openib_if_include mlx5_0:1 singularity exec --nv ../../../torch.simg python run.py
