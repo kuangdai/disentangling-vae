dc4f654735091e2e282b3a7c22362ebbdcf2d3d9
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2021-07-10 19:50:34 +0100

    evae run.sh

--- e_vae_proj/evae_dsprites/run_rec.sh
+++ e_vae_proj/evae_dsprites/run_rec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p pearl
-#SBATCH --job-name=bvae
+#SBATCH --job-name=evae_rec
 #SBATCH --mem-per-cpu=32G
 #SBATCH --mem-per-gpu=32G
 #SBATCH --threads-per-core=1
@@ -9,4 +9,4 @@
 #SBATCH -t 5-00:00:00
 
 module load OpenMPI/4.1.0-GCC-9.3.0
-mpirun --mca btl_openib_allow_ib 1 --mca btl_openib_if_include mlx5_0:1 singularity exec --nv ../../../torch.simg python run.py
+mpirun --mca btl_openib_allow_ib 1 --mca btl_openib_if_include mlx5_0:1 singularity exec --nv ../../../torch.simg python run.py rec
