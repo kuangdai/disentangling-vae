38d4ccf905e2ffb7af883da17119ed05734e7074
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2021-07-13 10:55:11 +0100

    Update collect_history.py

--- e_vae_proj/evae_dsprites/collect_history.py
+++ e_vae_proj/evae_dsprites/collect_history.py
@@ -19,25 +19,30 @@ if __name__ == '__main__':
 
     # sizes
     epochs = 50
-    batchs_per_epoch = len(range(0, 737280, 256))
+    record_every = 50
+    batchs = len(range(0, 737280 * epochs, 256))
+    records = len(range(0, batchs, record_every))
 
     # results
-    hist_rec = np.zeros((len(epses), len(nlats), epochs, batchs_per_epoch))
-    hist_KL = np.zeros((len(epses), len(nlats), epochs, batchs_per_epoch))
-    hist_loss = np.zeros((len(epses), len(nlats), epochs, batchs_per_epoch))
-    hist_lambda = np.zeros((len(epses), len(nlats), epochs, batchs_per_epoch))
+    hist_rec = np.zeros((len(epses), len(nlats), epochs, records))
+    hist_KL = np.zeros((len(epses), len(nlats), epochs, records))
+    hist_loss = np.zeros((len(epses), len(nlats), epochs, records))
+    hist_lambda = np.zeros((len(epses), len(nlats), epochs, records))
     for ieps, eps in enumerate(epses):
         for inlat, nlat in enumerate(nlats):
             res_dir = main_path / (f'results/evae_dsprites/{cons}/'
                                    f'z%d_e%s_s{seed}/' % (nlat, str(eps)))
             # collect history
+            row = 0
             for epoch in range(epochs):
                 fname = res_dir / f'train_losses_epoch{epoch}.log'
                 data = np.loadtxt(fname, skiprows=1)
-                hist_rec[ieps, inlat, epoch, :] = data[:, 1]
-                hist_KL[ieps, inlat, epoch, :] = data[:, 2]
-                hist_loss[ieps, inlat, epoch, :] = data[:, -3]
-                hist_lambda[ieps, inlat, epoch, :] = data[:, -2]
+                n = data.shape[0]
+                hist_rec[ieps, inlat, row:row + n] = data[:, 1]
+                hist_KL[ieps, inlat, row:row + n] = data[:, 2]
+                hist_loss[ieps, inlat, row:row + n] = data[:, -3]
+                hist_lambda[ieps, inlat, row:row + n] = data[:, -2]
+                row += n
                 print(f'DONE: {epoch}', end='\r')
             print(f'DONE: {res_dir}')
 
