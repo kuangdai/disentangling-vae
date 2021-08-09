c6b665d83ba0db934b78fbaf74b20f392ad68a3a
James Acris <james.acris@stfc.ac.uk>
2021-08-09 16:47:29 +0100

    finalise next set of epsvae jobs

--- e_vae_proj/qualitative/gen_jobs_2.py
+++ e_vae_proj/qualitative/gen_jobs_2.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
 
     # cherry-pick data samples as done in repo
     cherry_picked = ["92595 339150 656090", 
-                     "40919 5172 22330", 
-                     "88413 176606 179144 32260 191281 143307 101535 70059 87889 131612"]
+                     "88413 176606 179144 32260 191281 143307 101535 70059 87889 131612",
+                     "40919 5172 22330", ]
 
     # .sh filename
     fname = my_path / f'run_jobs_2.sh'
@@ -37,7 +37,8 @@ if __name__ == "__main__":
     # clear .sh file
     os.system(f'rm {fname}')
 
-    # multi-epsilon-VAE
+
+    # multi-epsilon-VAE from chaos
     for data, epochs, cherries in zip(datasets, epochs_list, cherry_picked):
         if data=="celeba":
             eps_Bs = eps_Bs_celeba
@@ -47,15 +48,39 @@ if __name__ == "__main__":
             for eps_G in eps_Gs:
                 for eps_B in eps_Bs:
 
-                    BTC_cmd = (
-                        # f"python main.py qualitative/btcvae_{data}_z{nlat}_A{alpha_gamma}_B{beta}_G{alpha_gamma} -s {seed} "
-                        # f"--checkpoint-every 50 -d {data} -e {epochs} -b {batchs} "
-                        # f"-z {nlat} -l btcvae --lr {lr} --btcvae-A {alpha_gamma} --btcvae-B {beta} --btcvae-G {alpha_gamma} "
-                        # f'--no-progress-bar -F {str(my_path / f"btcvae_{data}_z{nlat}_A{alpha_gamma}_B{beta}_G{alpha_gamma}.out")} '
-                        # f"--record-loss-every=50 --pin-dataset-gpu \n"
-                        f"python main_viz.py qualitative/btcvae_{data}_z{nlat}_A{alpha_gamma}_B{beta}_G{alpha_gamma} all -i {cherries} "
+                    cmd = (
+                        f"python main.py qualitative/mepsvae_{data}_z{nlat}_epsA{eps_A}_epsB{eps_B}_epsG{eps_G} -s {seed} "
+                        f"--checkpoint-every 50 -d {data} -e {epochs} -b {batchs} "
+                        f"-z {nlat} -l mepsvae --lr {lr} --mepsvae-epsilon-alpha {eps_A} --mepsvae-epsilon-beta {eps_B} --mepsvae-epsilon-gamma {eps_G}"
+                        f'--no-progress-bar -F {str(my_path / f"mepsvae_{data}_z{nlat}_epsA{eps_A}_epsB{eps_B}_epsG{eps_G}.out")} '
+                        f"--record-loss-every=50 --pin-dataset-gpu \n"
+                        f"python main_viz.py qualitative/mepsvae_{data}_z{nlat}_epsA{eps_A}_epsB{eps_B}_epsG{eps_G} all -i {cherries} "
                         f"-s {seed} -c 10 -r 10 -t 2 --is-show-loss --is-posterior \n"
                     )
 
                     with open(fname, 'a') as f:
-                        f.write(BTC_cmd)
+                        f.write(cmd)
+
+
+    # multi-epsilon-VAE from VAE
+    for data, epochs, cherries in zip(datasets, epochs_list, cherry_picked):
+        if data=="celeba":
+            eps_Bs = eps_Bs_celeba
+        else:
+            eps_Bs = eps_Bs_dsprites_chairs
+        for eps_A in eps_As:
+            for eps_G in eps_Gs:
+                for eps_B in eps_Bs:
+
+                    cmd = (
+                        f"python main.py qualitative/mepsvae_fromVAE_{data}_z{nlat}_epsA{eps_A}_epsB{eps_B}_epsG{eps_G} -s {seed} "
+                        f"--checkpoint-every 50 -d {data} -e {epochs} -b {batchs} --mepsvae-warmup 0 --continue-train-from VAE_{data}_z64 "
+                        f"-z {nlat} -l mepsvae --lr {lr} --mepsvae-epsilon-alpha {eps_A} --mepsvae-epsilon-beta {eps_B} --mepsvae-epsilon-gamma {eps_G}"
+                        f'--no-progress-bar -F {str(my_path / f"mepsvae_fromVAE_{data}_z{nlat}_epsA{eps_A}_epsB{eps_B}_epsG{eps_G}.out")} '
+                        f"--record-loss-every=50 --pin-dataset-gpu \n"
+                        f"python main_viz.py qualitative/mepsvae_fromVAE_{data}_z{nlat}_epsA{eps_A}_epsB{eps_B}_epsG{eps_G} all -i {cherries} "
+                        f"-s {seed} -c 10 -r 10 -t 2 --is-show-loss --is-posterior \n"
+                    )
+
+                    with open(fname, 'a') as f:
+                        f.write(cmd)
